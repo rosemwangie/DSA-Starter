e0106b3f08b6886679c879deca383dfe912e0ecc
rwambui <rosemwangi305@gmail.com>
2022-07-25 21:49:40 +0300

    get value by idx test

--- src/data-structures/linked-lists/app.js
+++ src/data-structures/linked-lists/app.js
@@ -1,8 +1,11 @@
 const LinkedList = require('./LinkedList')
 
 const myLinkedList = new LinkedList()
+myLinkedList.prepend(600);
+myLinkedList.prepend(500);
+myLinkedList.prepend(400);
 myLinkedList.prepend(300);
 myLinkedList.prepend(200);
-
-
-console.log(myLinkedList);
+myLinkedList.prepend(100);
+myLinkedList.printList();
+// console.log(myLinkedList);
