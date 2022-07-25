bbbf7e29253c53e1d11017220f11f07e6bfa09e5
rwambui <rosemwangi305@gmail.com>
2022-07-25 21:47:24 +0300

    get value by index method

--- src/data-structures/linked-lists/LinkedList.js
+++ src/data-structures/linked-lists/LinkedList.js
@@ -3,19 +3,33 @@ class LinkedList{
     this.head = null;
     this.size = 0;
   }
-  //Prepend (beginning)
+  //Prepend (beginning)  O(1)
   prepend(value){
     const newNode = new LinkedListNode(value, this.head);
     this.head = newNode;
     this.size++;
   }
-  //Print List (value)
-  //Append (end)
-  //InsertAt(value, index)
-  //GetAt (index)
-  //DeleteAt (index)
-  //ClearList
 
+  //Print List (value) O(n)
+  getByIndex(index) {
+    if (index < 0 || index >= this.length) return null
+
+    let current = this.head
+    for (let i = 0; i < index; i++) {
+      current = current.next
+    }
+    return current
+  }
+
+  printList() {
+    let output = ''
+    let current = this.head
+    while (current) {
+      output = `${output}${current.value} -> `
+      current = current.next
+    }
+    console.log(`${output}null`)
+  }
 }
 
 class LinkedListNode{
@@ -23,6 +37,14 @@ class LinkedListNode{
     this.value = value
     this.next = next
   }
+}
+
+LinkedList.fromValues = (...values) => {
+  const myLinkedList = new LinkedList()
+  for (let i = values.length -1; i >=0; i--) {
+    myLinkedList.prepend(values[i]);    
+  }
+  return myLinkedList;
 }
 
 module.exports = LinkedList;
