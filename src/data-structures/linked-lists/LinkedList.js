414801cdf25fed6f06e5cad64c4a19d704a0da64
rwambui <rosemwangi305@gmail.com>
2022-08-03 22:54:05 +0300

    insert method - ll

--- src/data-structures/linked-lists/LinkedList.js
+++ src/data-structures/linked-lists/LinkedList.js
@@ -30,6 +30,36 @@ class LinkedList{
     }
     console.log(`${output}null`)
   }
+
+  //insert
+  insert(value, rawIndex) {
+    const index = rawIndex < 0 ? 0 : rawIndex;
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      let count = 1;
+      let currentNode = this.head;
+      const newNode = new LinkedListNode(value);
+      while (currentNode) {
+        if (count === index) break;
+        currentNode = currentNode.next;
+        count += 1;
+      }
+      if (currentNode) {
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+      } else {
+        if (this.tail) {
+          this.tail.next = newNode;
+          this.tail = newNode;
+        } else {
+          this.head = newNode;
+          this.tail = newNode;
+        }
+      }
+    }
+    return this;
+  }
 }
 
 class LinkedListNode{
