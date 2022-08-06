a3dffa629747746a1fdcce68a94b220885504e1d
rwambui <rosemwangi305@gmail.com>
2022-08-06 13:57:52 +0300

    1. Two Sum

--- src/leetcode/1.TwoSum.js
+++ src/leetcode/1.TwoSum.js
@@ -1,4 +1,28 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+*/
+
+//brute force/naive approach
+
 const twoSum = (nums, target) => {
+    for(let i = 0; i < nums.length; i++){
+      for(let j = i + 1; j < nums.length; j++){
+        if(nums[i] + nums[j] === target){
+          return [i,j]
+        }
+      }
+    }
+    return [];
+}
+
+
+
+
+
+// Optimal soln 1
+const twoSum1 = (nums, target) => {
   const hash = {}; 
   for (let i = 0; i < nums.length; i++) {
     const n = nums[i];
@@ -9,4 +33,25 @@ const twoSum = (nums, target) => {
   }
   return []; 
  }
- module.exports = twoSum;
+
+
+
+
+
+
+ //Optimal Soln 2
+
+ const twoSum2 = (nums, target) => {
+    const map = nums.reduce((acc, current, idx) => {
+      acc[current] = idx;
+      return acc;
+    }, {})
+    for(let i=0; i < nums.length; i++){
+      const missingTarget = target - nums[i];
+      if(map[missingTarget]){
+        return [i,map[missingTarget]];
+      }
+    }
+ }
+
+ module.exports = twoSum; 
