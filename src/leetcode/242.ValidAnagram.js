7c602c828c01f5ab2d194681b828b0b30d6b4ab6
rwambui <rosemwangi305@gmail.com>
2022-08-11 21:20:56 +0300

    242.Valid Anagram

--- src/leetcode/242.ValidAnagram.js
+++ src/leetcode/242.ValidAnagram.js
@@ -2,7 +2,7 @@
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
 typically using all the original letters exactly once.
-
+Example:
 Input: s = "rat", t = "tar"
 Output: false 
 */
