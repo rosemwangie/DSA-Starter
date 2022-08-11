065885097756c578cf6b5eb62c3c538324cc1b3e
rwambui <rosemwangi305@gmail.com>
2022-08-11 21:02:31 +0300

    242. Valid Anagram

---
+++ src/leetcode/242.ValidAnagram.js
@@ -0,0 +1,34 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+Input: s = "rat", t = "tar"
+Output: false 
+*/
+
+// O(n) T O(n) Space
+const isAnagram = (s, t) => {
+  if (s.length != s.length) return false;
+  const charObj = {};
+  for (char of s) {
+    if (charObj[char]) {
+      charObj++;
+    } else {
+      charObj[char] = 1;
+    }
+  }
+  for (char of t) {
+    if (charObj[char]) {
+      charObj[char]--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(validAnagram1("anagram", "nagaram"));
+console.log(validAnagram1("anagrm", "nagaram"));
+console.log(validAnagram1("", "nagaram"));
+console.log(validAnagram1("ab", "a"));
