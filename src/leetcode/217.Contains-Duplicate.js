a344f9ee7f56f0db92f2c03c0faec3a425e68d12
rwambui <rosemwangi305@gmail.com>
2022-08-12 18:17:14 +0300

    217.Contains Duplicates

--- src/leetcode/217.Contains-Duplicate.js
+++ src/leetcode/217.Contains-Duplicate.js
@@ -14,31 +14,30 @@ const containsDuplicate = (nums) => {
     }
   }
   return false;
-}
+};
 
 // Optimal solution 1, using sorting Time Complexity: O(nlogn), Space Complexity: O(1)
 const containDuplicate = (nums) => {
   if (nums.length <= 0) return false;
   nums.sort((a, b) => a - b);
-  for(let i = 0; i < nums.length; i++){
-    if(nums[i] === nums[i + 1]){
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
       return true;
     }
   }
   return false;
-}
+};
 
 // Optimal solution 2, using hashmaps Time Complexity: O(n), Space Complexity: O(n)
-const containDuplicate1 = (nums) =>{
+const containDuplicate1 = (nums) => {
   let map = new Map();
-  for(num of nums){
-    if(map.has(num)) return true;
+  for (num of nums) {
+    if (map.has(num)) return true;
     map.set(num);
   }
   return false;
-}
-
-
+};
 // console.log(containsDuplicate([1,2,3,1]));
 // console.log(containDuplicate([1,2,3,4]));
 // console.log(containsDuplicate1([1,1,1,3,3,4,3,2,4,2]));
+// console.log(containsDuplicate1([]));
