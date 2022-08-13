27eafba141c6ceb96246ded3fbe99320132e543f
rwambui <rosemwangi305@gmail.com>
2022-08-13 11:53:58 +0300

    created 11.Container With most water

--- src/leetcode/11.ContainerWithMostWater.js
+++ src/leetcode/11.ContainerWithMostWater.js
@@ -1,16 +1,28 @@
-const search = (nums, target) => {
-  const len = nums.length;
-  let leftIdx = 0; 
-  let rightIdx =  len - 1;
-  while(leftIdx <= rightIdx){
-      let midIdx = Math.floor((leftIdx + rightIdx)/2);
-      if(target == nums[midIdx]){
-          return midIdx;
-      } else if (target < nums[midIdx]){
-          rightIdx = midIdx - 1;
-      } else {
-          leftIdx = midIdx + 1;
-      }
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container.
+*/
+
+//
+const maxArea = (height) => {
+  const len = height.length;
+  let leftPointer = 0,
+    rightPointer = len - 1,
+    calMaxArea = 0;
+  while (leftPointer < rightPointer) {
+    const heights = Math.min(height[leftPointer], height[rightPointer]);
+    const width = rightPointer - leftPointer;
+    const area = heights * width;
+    calMaxArea = Math.max(calMaxArea, area);
+
+    if (height[leftPointer] <= height[rightPointer]) {
+      leftPointer++;
+    } else {
+      rightPointer--;
+    }
   }
-  return -1
-}
+
+  return calMaxArea;
+};
