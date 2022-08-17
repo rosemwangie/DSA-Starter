63a5718a83c5e49e38d11f36a91d3a4985374348
rwambui <rosemwangi305@gmail.com>
2022-08-17 20:01:34 +0300

    created 1.Two Sum

--- src/leetcode/1.TwoSum.js
+++ src/leetcode/1.TwoSum.js
@@ -4,45 +4,45 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 */
 
-//brute force/naive approach
-
+//brute force/naive approach Time complexity O(n^2) Space complexity O(1)
 const twoSum = (nums, target) => {
-    for(let i = 0; i < nums.length; i++){
-      for(let j = i + 1; j < nums.length; j++){
-        if(nums[i] + nums[j] === target){
-          return [i,j]
-        }
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
       }
     }
-    return [];
-}
+  }
+  return [];
+};
 
-// Optimal soln 1
+// Optimal soln 1 Time complexity O(n) Space complexity O(n)
 const twoSum1 = (nums, target) => {
-  const hash = {}; 
+  const hash = {};
   for (let i = 0; i < nums.length; i++) {
     const n = nums[i];
-    if (hash[target - n] !== undefined) { 
-      return [hash[target - n], i]; 
+    if (hash[target - n] !== undefined) {
+      return [hash[target - n], i];
     }
-    hash[n] = i; 
+    hash[n] = i;
   }
-  return []; 
- }
+  return [];
+};
 
- //Optimal Soln 2
-
- const twoSum2 = (nums, target) => {
-    const map = nums.reduce((acc, current, idx) => {
-      acc[current] = idx;
-      return acc;
-    }, {})
-    for(let i=0; i < nums.length; i++){
-      const missingTarget = target - nums[i];
-      if(map[missingTarget]){
-        return [i,map[missingTarget]];
-      }
+//Optimal Soln 2 Time complexity O(n) Space complexity O(1)
+const twoSum2 = (nums, target) => {
+  const map = nums.reduce((acc, current, idx) => {
+    acc[current] = idx;
+    return acc;
+  }, {});
+  for (let i = 0; i < nums.length; i++) {
+    const missingTarget = target - nums[i];
+    if (map[missingTarget]) {
+      return [i, map[missingTarget]];
     }
- }
+  }
+};
 
- module.exports = twoSum; 
+console.log(twoSum([2, 7, 11, 15], 9)); //[0,1]
+console.log(twoSum([3, 2, 4], 6)); //[1,2]
+console.log(twoSum([3, 3], 6)); //[0,1]
