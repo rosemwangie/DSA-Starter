2b68a7e622897430e2bb3c9cf64bd2ea047335f2
rwambui <rosemwangi305@gmail.com>
2022-08-18 18:15:05 +0300

    created 34.Find first and last position of element in sorted Array

--- src/leetcode/34.Find-First-and-Last-Position-of-Element-in-Sorted-Array.js
+++ src/leetcode/34.Find-First-and-Last-Position-of-Element-in-Sorted-Array.js
@@ -1,41 +1,50 @@
+/*Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+*/
+//Optimal solution TIme complexity O(log n) Space complexity O(1)
 const findFirstAndLast = (nums, target) => {
-  const first = firstEl(nums, target);  
+  const first = firstEl(nums, target);
   const last = lastEl(nums, target, first);
   return [first, last];
-}
+};
 
 function firstEl(nums, target) {
-      let leftPointer = 0;
-      let rightPointer = nums.length - 1;
-      let firstPosition = -1;
-    while(leftPointer <= rightPointer) {
-      let mid = Math.floor((leftPointer + rightPointer)/2);
-      if(nums[mid] >= target) {
-        if(nums[mid] === target){
-          firstPosition = mid;
-        }
-        rightPointer = mid - 1;        
-      } else {
-        leftPointer = mid + 1;
+  let leftPointer = 0;
+  let rightPointer = nums.length - 1;
+  let firstPosition = -1;
+  while (leftPointer <= rightPointer) {
+    let mid = Math.floor((leftPointer + rightPointer) / 2);
+    if (nums[mid] >= target) {
+      if (nums[mid] === target) {
+        firstPosition = mid;
       }
+      rightPointer = mid - 1;
+    } else {
+      leftPointer = mid + 1;
     }
-       return firstPosition;    
   }
-  
-function lastEl(nums, target, first){
-      let leftPointer = first;
-      let rightPointer = nums.length - 1;
-      let lastPosition = first;
-    while(leftPointer <= rightPointer) {
-      let mid = Math.floor((leftPointer + rightPointer)/2);
-      if(nums[mid] <= target) {
-        if(nums[mid] === target){
-          lastPosition = mid;
-        }
-        leftPointer = mid + 1;                
-      } else {
-        rightPointer = mid - 1;
+  return firstPosition;
+}
+
+function lastEl(nums, target, first) {
+  let leftPointer = first;
+  let rightPointer = nums.length - 1;
+  let lastPosition = first;
+  while (leftPointer <= rightPointer) {
+    let mid = Math.floor((leftPointer + rightPointer) / 2);
+    if (nums[mid] <= target) {
+      if (nums[mid] === target) {
+        lastPosition = mid;
       }
+      leftPointer = mid + 1;
+    } else {
+      rightPointer = mid - 1;
     }
-       return lastPosition;
   }
+  return lastPosition;
+}
+
+console.log(findFirstAndLast([5, 7, 7, 8, 8, 10]), 8); //[3,4]
+console.log(findFirstAndLast([5, 7, 7, 8, 8, 10]), 6); // [-1,-1]
+console.log(findFirstAndLast([])); //[-1,-1]
