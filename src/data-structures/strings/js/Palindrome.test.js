dbc018f34beb2081032829b733bf7a7e46f6ac74
Rose Mwangi <rosemwangi305@gmail.com>
2022-10-11 23:14:12 +0300

    Update Palindrome.test.js

--- src/data-structures/strings/js/Palindrome.test.js
+++ src/data-structures/strings/js/Palindrome.test.js
@@ -1,15 +1,15 @@
 const palindrome = require("./Palindrome");
 
 describe("palindromeCheck", () => {
-  it("should return whether or not the string is a palindrome", () => {
+  it("will return whether or not the string is a palindrome", () => {
     expect(palindrome("A man, a plan, a canal: Panama")).toBe(true);
     expect(palindrome("racecar")).toBe(true);
     expect(palindrome("peep")).toBe(true);
     expect(palindrome("mom")).toBe(true);
     expect(palindrome("")).toBe(true);
-
     expect(palindrome("car")).toBe(false);
     expect(palindrome("geeks")).toBe(false);
     expect(palindrome("hunt")).toBe(false);
   });
 });
+
