1d30892b150fff78300c69ef9c82334633c39279
Rose Mwangi <rosemwangi305@gmail.com>
2022-11-22 23:03:00 +0300

    Update StackLinkedList.js

--- src/data-structures/stacks/js/StackLinkedList.js
+++ src/data-structures/stacks/js/StackLinkedList.js
@@ -1,12 +1,51 @@
-import LinkedList from "../linked-lists/LinkedList";
-
-export default class Stack {
-  constructor() {
-    this.LinkedList = new LinkedList();
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  push() {}
-  pop() {}
-  peek() {}
-  isEmptty() {}
-  print() {}
 }
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.size = 0;
+  }
+
+  pop() {
+
+  }
+  push(value) {
+    const node = new Node(value);
+
+    //empty
+    if (!this.top) {
+      this.top = node;
+      this.bottom = node;
+    } else {
+      //!empty
+      let temp = this.top;
+      this.top = node;
+      this.top.next = temp;
+    }
+    this.size++;
+    return this;
+
+  }
+  peek() {
+
+  }
+  isEmpty() {
+
+  }
+  search() {
+
+  }
+}
+
+const stack = new Stack();
+stack.push("900");
+stack.push("800");
+stack.push("700");
+
+console.log(stack);
