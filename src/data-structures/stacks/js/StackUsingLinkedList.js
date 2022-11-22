8c7c30d41febe07f86047b509ca83c81af106551
Rose Mwangi <rosemwangi305@gmail.com>
2022-11-22 23:52:32 +0300

    Update and rename StackLinkedList.js to StackUsingLinkedList.js

--- src/data-structures/stacks/js/StackUsingLinkedList.js
+++ src/data-structures/stacks/js/StackUsingLinkedList.js
@@ -11,10 +11,27 @@ class Stack {
     this.bottom = null;
     this.size = 0;
   }
-
+  //pop
   pop() {
-
+    if (!this.top) {
+      return -1;
+    }
+    if (this.length === 1) {
+      let top = this.top;
+      this.top = null;
+      this.bottom = null;
+      this.size--;
+      return top;
+    } else {
+      let top = this.top;
+      this.top = this.top.next;
+      this.size--;
+      return top;
+    }
+    return this;
   }
+
+  //push
   push(value) {
     const node = new Node(value);
 
@@ -30,22 +47,17 @@ class Stack {
     }
     this.size++;
     return this;
-
   }
-  peek() {
-
-  }
-  isEmpty() {
-
-  }
-  search() {
-
-  }
+  peek() {}
+  isEmpty() {}
+  search() {}
 }
 
 const stack = new Stack();
 stack.push("900");
 stack.push("800");
 stack.push("700");
+stack.pop();
+stack.pop();
 
 console.log(stack);
