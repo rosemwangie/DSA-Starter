80941762f5d740088fb00cb012ef2493d998c115
WalobwaD <walobwadan@gmail.com>
2022-12-08 19:19:41 +0300

    Leetcode 189

---
+++ src/leetcode/python/189.Rotate-Array.py
@@ -0,0 +1,26 @@
+'''Given an array, rotate the array to the right by k steps, where k is non-negative.'''
+
+class Solution:
+
+    #brute force/naive approach Time complexity O(n) Space complexity O(k)
+    def rotate1(self, nums: List[int], k: int)-> List[int]:
+        k = k%nums.length
+        temp = nums[-k:]
+        for i in reversed(range(0, len(nums)-k)):
+            nums[i+k] = nums[i]
+        for i in range(len(temp)):
+            nums[i] = temp[i]
+        return nums
+
+    #Optimal soln1 Time complexity O(n) Space complexity O(1)
+    def reverse(self, arr, start, end):                     #helper function(optional!)
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+        return arr
+
+    def rotate2(self, nums:List[int], k: int) -> List[int]:
+        k = k%len(nums)
+        reverse(nums, 0, len(nums)-)
+        reverse(nums, 0, k-1)
+        reverse(nums, k, len(nums)-1)
+        return nums
