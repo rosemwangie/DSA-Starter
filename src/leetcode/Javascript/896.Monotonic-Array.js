2d6eecb166611332a01dfe02d738fe6a66bea5c0
Rose Mwangi <rosemwangi305@gmail.com>
2022-12-14 21:46:05 +0300

    created monotonic array

---
+++ src/leetcode/Javascript/896.Monotonic-Array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+/**
+ * An array is monotonic if it is either monotone increasing or monotone decreasing
+ */
+
+const isMonotonic = (nums) => {
+  if (nums.length < 2) return true;
+
+  let goingUp = true;
+  let goingDown = true;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] > nums[i]) goingUp = false;
+    if (nums[i - 1] < nums[i]) goingDown = false;
+  }
+
+  return goingUp || goingDown;
+};
+
+console.log(isMonotonic([1, 2, 2, 3])); //true
+console.log(isMonotonic([6, 5, 4, 4])); //true
+console.log(isMonotonic([1, 3, 2])); //false
