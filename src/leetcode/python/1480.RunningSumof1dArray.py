a5d18fb72017cb533d7a38586640350badd10243
WalobwaD <walobwadan@gmail.com>
2022-12-15 17:57:35 +0300

    Modified Leetcode 1480

--- src/leetcode/python/1480.RunningSumof1dArray.py
+++ src/leetcode/python/1480.RunningSumof1dArray.py
@@ -1,10 +1,22 @@
-def runningSum(self, nums: List[int]) -> List[int]:
     """
     Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
     Return the running sum of nums.
 
     This algorithm uses constant extra space
     """
+class Solution:
+    #optimal sln Time Complexity O(n) Space Complexity O(1)
+    def runningSum(self, nums: List[int]) -> List[int]:
+
     for i in range(1, len(nums)):
-        nums[i] += nums[i - 1]
+        nums[i] += nums[i-1]
+        
     return nums
+
+    #or
+    # def runningSum(self, nums:List[int]) -> List[int]:
+    #     res = 0 
+    #     for i in range(len(nums)):
+    #         res += nums[i]
+    #         nums[i] = res
+    #     return nums
