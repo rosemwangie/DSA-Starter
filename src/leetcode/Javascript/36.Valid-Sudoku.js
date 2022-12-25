7a33407ff262e58f7473c9e69b2b7adfe489253d
Rose Mwangi <rosemwangi305@gmail.com>
2022-12-25 22:50:10 +0300

    valid sudoku

---
+++ src/leetcode/Javascript/36.Valid-Sudoku.js
@@ -0,0 +1,8 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+
+const isValidSudoku = () => {
+  //create an array to store if. each row, column , and sub-box has seen a certain number
+}
