42da287f59e104ce62508bab2f3bda598e3f4390
WalobwaD <walobwadan@gmail.com>
2023-01-09 07:01:10 +0300

    Modified solutions

--- src/leetcode/python/34.Find-First-and-Last-Position-of-Element-in-Sorted-Array.py
+++ src/leetcode/python/34.Find-First-and-Last-Position-of-Element-in-Sorted-Array.py
@@ -52,9 +52,9 @@ class Solution:
 
         return -1
 
-
-print(findFirstAndLast([5, 7, 7, 8, 8, 10]), 8); #[3,4]
-print(findFirstAndLast([5, 7, 7, 8, 8, 10]), 6); # [-1,-1]
-print(findFirstAndLast([])); #[-1,-1]
+s = Solution()
+print(s.findFirstAndLast([5, 7, 7, 8, 8, 10]), 8); #[3,4]
+print(s.findFirstAndLast([5, 7, 7, 8, 8, 10]), 6); # [-1,-1]
+print(s.findFirstAndLast([])); #[-1,-1]
 
 
