79d887358e8d0b7408121798cd1a5c0dfae297fd
WalobwaD <walobwadan@gmail.com>
2023-01-09 07:29:12 +0300

    solutions

--- src/leetcode/python/704.Binary-Search.py
+++ src/leetcode/python/704.Binary-Search.py
@@ -1,3 +1,10 @@
+'''
+    Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+'''
 class Solution:
 
     #Optimal Soln  O(logn)
@@ -7,9 +14,9 @@ class Solution:
         right = len(nums)-1
 
         while left <= right:
-             midNum = round(( left + right ) // 2)
+            midNum = round(( left + right ) // 2)
 
-             if nums[midNum] == target:
+            if nums[midNum] == target:
                 return midNum
             elif target < nums[midNum]:
                 right = midNum -1
@@ -17,3 +24,6 @@ class Solution:
                 left = midNum +1
         return -1
 
+s = Solution()
+print(s.binarySearch([-1,0,3,5,9,12], 9))   #4
+print(s.binarySearch([-1,0,3,5,9,12], 2))   #-1
