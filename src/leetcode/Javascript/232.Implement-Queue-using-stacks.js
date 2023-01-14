a82eddc10891bad7b7cb7ddf4cfad4a77075728f
Rose Mwangi <rosemwangi305@gmail.com>
2023-01-14 11:14:53 +0300

    Delete 232.Implement-Queue-using-stacks.js

--- src/leetcode/Javascript/232.Implement-Queue-using-stacks.js
+++
@@ -1,3 +0,0 @@
-/**
- * Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
- */
