dbf7c8367f01ba9338085e58c361657ed30448b8
Nyandika Donald <donald.nyandika@gmail.com>
2023-02-27 17:56:29 +0000

    Add binary search

--- src/algorithms/search/js/Binary Search/binary-search-implementation.js
+++ src/algorithms/search/js/Binary Search/binary-search-implementation.js
@@ -1,2 +1,43 @@
-//Binary search implementation
-//Psuedo
+class BinarySearch {
+  // binary search only works on sorted arrays
+  search(sortedArray, target, comparator) {
+    let start = 0;
+    let end = sortedArray.length - 1;
+    let mid = Math.floor((start + end) / 2);
+
+    // if a comparator is passed in, use it to compare the target and the middle element
+    if (comparator !== undefined) {
+      while (comparator(sortedArray[mid], target) !== 0 && start <= end) {
+        // if the target is less than the middle element, then the target is in the left half of the array
+        if (comparator(sortedArray[mid], target) === -1) {
+          end = mid - 1;
+        }
+        // if the target is greater than the middle element, then the target is in the right half of the array
+        else {
+          start = mid + 1;
+        }
+        mid = Math.floor((start + end) / 2);
+      }
+
+      // if the target is not found, return -1
+      return comparator(sortedArray[mid], target) === 0 ? mid : -1;
+    }
+
+    while (sortedArray[mid] !== target && start <= end) {
+      // if the target is less than the middle element, then the target is in the left half of the array
+      if (target < sortedArray[mid]) {
+        end = mid - 1;
+      }
+      // if the target is greater than the middle element, then the target is in the right half of the array
+      else {
+        start = mid + 1;
+      }
+      mid = Math.floor((start + end) / 2);
+    }
+
+    // if the target is not found, return -1
+    return sortedArray[mid] === target ? mid : -1;
+  }
+}
+
+module.exports = BinarySearch;
--- src/algorithms/search/js/Binary Search/binary-search.test.js
+++ src/algorithms/search/js/Binary Search/binary-search.test.js
@@ -1,16 +1,18 @@
-import binarySearch from '../binarySearch';
+const BinarySearch = require('./binary-search-implementation');
 
 describe('binarySearch', () => {
+  const binarySearch = new BinarySearch();
+
   test('should search number in sorted array', () => {
-    expect(binarySearch([], 1)).toBe(-1);
-    expect(binarySearch([1], 1)).toBe(0);
-    expect(binarySearch([1, 2], 1)).toBe(0);
-    expect(binarySearch([1, 2], 2)).toBe(1);
-    expect(binarySearch([1, 5, 10, 12], 1)).toBe(0);
-    expect(binarySearch([1, 2, 19, 29, 34, 37, 42, 104], 37)).toBe(5);
-    expect(binarySearch([1, 2, 19, 29, 34, 37, 42, 104], 1)).toBe(0);
-    expect(binarySearch([1, 2, 19, 29, 34, 37, 42, 104], 104)).toBe(7);
-    expect(binarySearch([1, 2, 19, 29, 34, 37, 42, 104], 0)).toBe(-1);
+    expect(binarySearch.search([], 1)).toBe(-1);
+    expect(binarySearch.search([1], 1)).toBe(0);
+    expect(binarySearch.search([1, 2], 1)).toBe(0);
+    expect(binarySearch.search([1, 2], 2)).toBe(1);
+    expect(binarySearch.search([1, 5, 10, 12], 1)).toBe(0);
+    expect(binarySearch.search([1, 2, 19, 29, 34, 37, 42, 104], 37)).toBe(5);
+    expect(binarySearch.search([1, 2, 19, 29, 34, 37, 42, 104], 1)).toBe(0);
+    expect(binarySearch.search([1, 2, 19, 29, 34, 37, 42, 104], 104)).toBe(7);
+    expect(binarySearch.search([1, 2, 19, 29, 34, 37, 42, 104], 0)).toBe(-1);
   });
 
   test('should search object in sorted array', () => {
@@ -25,10 +27,18 @@ describe('binarySearch', () => {
       return a.key < b.key ? -1 : 1;
     };
 
-    expect(binarySearch([], { key: 1 }, comparator)).toBe(-1);
-    expect(binarySearch(sortedArrayOfObjects, { key: 4 }, comparator)).toBe(-1);
-    expect(binarySearch(sortedArrayOfObjects, { key: 1 }, comparator)).toBe(0);
-    expect(binarySearch(sortedArrayOfObjects, { key: 2 }, comparator)).toBe(1);
-    expect(binarySearch(sortedArrayOfObjects, { key: 3 }, comparator)).toBe(2);
+    expect(binarySearch.search([], { key: 1 })).toBe(-1);
+    expect(
+      binarySearch.search(sortedArrayOfObjects, { key: 4 }, comparator)
+    ).toBe(-1);
+    expect(
+      binarySearch.search(sortedArrayOfObjects, { key: 1 }, comparator)
+    ).toBe(0);
+    expect(
+      binarySearch.search(sortedArrayOfObjects, { key: 2 }, comparator)
+    ).toBe(1);
+    expect(
+      binarySearch.search(sortedArrayOfObjects, { key: 3 }, comparator)
+    ).toBe(2);
   });
 });
