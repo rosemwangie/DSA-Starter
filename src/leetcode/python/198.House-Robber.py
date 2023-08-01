cb97267057f1c5c59d8e8f7d548ba7a83f3b44f3
WalobwaD <walobwadan@gmail.com>
2023-08-01 11:37:18 +0300

    Update BigO of Leet 198

--- src/leetcode/python/198.House-Robber.py
+++ src/leetcode/python/198.House-Robber.py
@@ -1,16 +1,22 @@
-# You are a professional robber planning to rob houses along a street. 
-# Each house has a certain amount of money stashed, the only constraint stopping you
-# from robbing each of them is that adjacent houses have security systems connected and 
-# it will automatically contact the police if two adjacent houses were broken into on the
-# same night.
+'''
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, the only constraint stopping you
+from robbing each of them is that adjacent houses have security systems connected and 
+it will automatically contact the police if two adjacent houses were broken into on the
+same night.
 
-# Given an integer array nums representing the amount of money of each house,
-# return the maximum amount of money you can rob tonight without alerting the police.
-
+Given an integer array nums representing the amount of money of each house,
+return the maximum amount of money you can rob tonight without alerting the police.
+'''
  
 
 class Solution:
 
+    '''
+    Optimal soln1
+    Time complexity O(n) 
+    Space complexity O(n)
+    '''
     def houseRobberMemo(self, nums):
         if len(nums) < 2:
             return nums[0]
@@ -24,6 +30,11 @@ class Solution:
         
         return memo[len(nums) -1]
     
+    '''
+    Optimal soln2
+    Time complexity O(n) 
+    Space complexity O(1)
+    '''
     def houseRobberBottomUp(self, nums):
         rob1, rob2 = 0, 0
 
